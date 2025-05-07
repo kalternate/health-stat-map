eb7f2f360562c3ae2fff14722a5974d68a490bea
DylanKCode <karterd@wwu.edu>
2025-05-07 00:04:40 -0700

    Got all the indicators for main 5 diseases

---
+++ getAll_Indicators_Malaria_Cholera_TB_HIV_AIDS.py
@@ -0,0 +1,90 @@
+malaria = {'MALARIA_NMCP_CALCULATIONS': 'Domestic Expenditure on Malaria', 
+           'MALARIA_PF_INDIG': 'Number of indigenous P. falciparum malaria cases', 
+           'MALARIA_INDIG_STATUS': 'Status of indigenous malaria cases', 
+           'MALARIA_SUSPECTS': 'Number of suspected malaria cases', 
+           'MALARIA_TOTAL_CASES': 'Total number of\xa0 malaria cases (presumed + confirmed cases)', 
+           'MALARIA_MICR_POS': 'Number of malaria positive cases by microscopy', 
+           'MALARIA_PV_INDIG': 'Number of indigenous P. vivax malaria cases', 
+           'MALARIA_INDIG': 'Number of indigenous malaria cases', 
+           'MALARIA_EST_CASES': 'Estimated number of malaria cases', 
+           'MALARIA_IPTP3_COVERAGE': 'Pregnant women attending antenatal care\xa0 at least once and receiving at least 3 doses of Intermittent Preventive Treatment of Malaria for Pregnant Women (IPTp3) (%)', 
+           'MALARIA_EST_MORTALITY': 'Estimated malaria mortality rate (per 100 000 population)', 
+           'MALARIA_IRS_COVERAGE': 'Number of people protected from malaria by indoor residual spraying (IRS)', 
+           'MALARIA_RDT_TEST': 'Number of malaria suspects examined by rapid diagnostic test (RDT)', 
+           'MALARIA_CONF_CASES': 'Number of confirmed malaria cases', 
+           'MALARIA_ITN_COVERAGE': 'Population with access to an insecticide-treated bed net (ITN) for malaria protection (%), modelled', 
+           'MALARIA_EST_DEATHS': 'Estimated number of malaria deaths', 
+           'MALARIA_PRES_CASES': 'Number of presumed malaria cases', 
+           'MALARIA_IMPORTED': 'Number of imported malaria cases', 
+           'MALARIA_EST_INCIDENCE': 'Estimated malaria incidence (per 1000 population at risk)', 
+           'MALARIA_MICR_TEST': 'Number of malaria suspects examined by microscopy', 
+           'MALARIA_RDT_POS': 'Number of malaria positive cases by rapid diagnostic test (RDT)', 
+           'MALARIA_1STLINE_TREATED': 'Number of malaria cases treated with any first\xa0 line tx courses (including artemisinin-based combination therapies (ACTs))', 
+           'MALARIA_ACT_TREATED': 'Number of malaria cases treated with artemisinin-based combination therapies (ACTs)'}
+
+
+
+cholera = {'CHOLERA_0000000002': 'Number of reported deaths from cholera', 
+           'CHOLERA_0000000001': 'Number of reported cases of cholera', 
+           'CHOLERA_0000000003': 'Cholera case fatality rate'}#3
+
+
+
+tuberculosis = {'MDG_0000000017': 'Deaths due to tuberculosis among HIV-negative people (per 100 000 population)', 
+                'TB_e_inc_num': 'Number of incident tuberculosis cases', 
+                'TB_c_newinc': 'Tuberculosis - new and relapse cases', 
+                'TB_c_lab_cul_5m': 'Laboratories providing tuberculosis diagnostic services using culture (per 5 million population)', 
+                'MDG_0000000030': 'Smear-positive tuberculosis case-detection rate (%)', 
+                'TB_tot_newrel': 'Tuberculosis - new and relapse cases', 
+                'TB_Notification_agesex_num': 'The number of new and relapse cases of tuberculosis detected in a given year by age and sex', 
+                'TB_e_mort_exc_tbhiv_num': 'Number of deaths due to tuberculosis, excluding HIV', 
+                'TB_e_inc_tbhiv_num': 'Number of incident tuberculosis cases,  (HIV-positive cases)', 
+                'TB_1': 'Tuberculosis treatment coverage', 
+                'TB_c_lab_sm_100k': 'Laboratories providing tuberculosis diagnostic services using sputum smear microscopy (per 100 000 population)', 
+                'MDG_0000000020': 'Incidence of tuberculosis (per 100 000 population per year)', 
+                'MDG_0000000031': 'Smear-positive tuberculosis treatment-success rate (%)', 
+                'TB_e_prev_num': 'Number of prevalent tuberculosis cases', 
+                'TB_e_inc_num_014': 'Number of incident tuberculosis cases in children aged 0 - 14', 
+                'TB_e_inc_tbhiv_100k': 'Incidence of tuberculosis (per 100 000 population) (HIV-positive cases)'}
+
+
+
+HIV_AIDS = {'HIV_ARTCOVERAGE': 'Estimated antiretroviral therapy coverage among people living with HIV (%)', 
+            'HIV_0000000026': 'Number of new HIV infections', 
+            'MDG_0000000017': 'Deaths due to tuberculosis among HIV-negative people (per 100 000 population)', 
+            'RSUD_620': 'HIV, Hepatitis counselling and treatment availability in specialized facilities and services', 
+            'RSUD_41': 'Specialized treatment services for drug use disoders and HIV/AIDS', 
+            'TB_c_tbhiv_tsr': 'Treatment success rate: HIV-positive TB cases', 
+            'HIV_0000000020': 'Estimated number of pregnant women living with HIV', 
+            'HIV_0000000001': 'Estimated number of people (all ages) living with HIV', 
+            'MDG_0000000034': 'Antiretroviral therapy coverage among people with advanced HIV infection (%), WHO 2006 guidelines', 
+            'TB_hivtest_pos_pct': 'Tested TB patients HIV-positive (%)', 
+            'WHS2_138': 'Deaths due to HIV/AIDS (per 100 000 population)', 
+            'HHS_LAWPOLICY': 'HIV, Hepatitis, and STI-related laws and policies', 
+            'PRISON_D4_HIV_DIAG_TOT': 'In-prison HIV diagnosis (number of diagnoses)', 
+            'TB_hiv_cpt_pct': 'HIV-positive TB patients on CPT (co-trimoxazole preventive therapy) (%)', 
+            'TB_hivtest_pct': 'TB patients with known HIV status (%)', 
+            'HIV_0000000015': 'Number of pregnant women living with HIV who received antiretrovirals for preventing mother-to-child transmission', 
+            'TB_e_mort_exc_tbhiv_num': 'Number of deaths due to tuberculosis, excluding HIV', 
+            'HIV_0000000006': 'Number of people dying from HIV-related causes', 
+            'TB_e_inc_tbhiv_num': 'Number of incident tuberculosis cases,  (HIV-positive cases)', 
+            'HEPATITIS_HCV_TREATMENT_HIV_PERCENT': 'People living with HIV treated for chronic hepatitis C infection (percent)', 
+            'HIV_POSITIVITY': 'Percentage of HIV-positive results returned to people in the calendar year', 
+            'HEPATITIS_HCV_TESTING_HIV_PERCENT': 'People living with HIV diagnosed with chronic hepatitis C infection (percent)', 
+            'HIV_0000000027': 'Reported number of HIV tests performed where results were received by a person (testing volume)', 
+            'MDG_0000000029': 'Prevalence of HIV among adults aged 15 to 49 (%)', 
+            'SDGHIV': 'New HIV infections (per 1000 uninfected population)', 
+            'MDG_0000000021': 'Population aged 15-24 years with comprehensive correct knowledge of HIV/AIDS (%)', 
+            'TB_hiv_art_pct': 'HIV-positive TB patients on ART (antiretroviral therapy) (%)', 
+            'HEPATITIS_HCV_TREATMENT_PWID_PERCENT': 'People living with HIV who inject drugs, treated for chronic hepatitis C infection (percent)', 
+            'HIV_0000000021': 'Estimated number of pregnant women living with HIV needing antiretrovirals for preventing mother-to-child transmission', 
+            'HEPATITIS_HCV_TESTING_PWID_PERCENT': 'People living with HIV who inject drugs, diagnosed with chronic hepatitis C infection (percent)', 
+            'MDG_0000000033': 'Antiretroviral therapy coverage among people with HIV infection eligible for ART according to 2010 guidelines (%)', 
+            'TB_e_inc_tbhiv_100k': 'Incidence of tuberculosis (per 100 000 population) (HIV-positive cases)'}
+
+
+# for key, value in indi.items(): # Used indicators.json to find data indicators
+#     if "hiv" in value.lower() or "aids" in value.lower():
+#         HIV_AIDS[key] = value
+
+# print(HIV_AIDS)
